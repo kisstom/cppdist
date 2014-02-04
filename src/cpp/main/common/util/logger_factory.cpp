4427f5a39d5247d17cfc24fb0466011d76dbc135
kisstom# <kisstom@info.ilab.sztaki.hu>
2014-02-04 12:52:11 +0100

    adding logger factory

---
+++ src/cpp/main/common/util/logger_factory.cpp
@@ -0,0 +1,38 @@
+/*
+ * logger_factory.cpp
+ *
+ *  Created on: 2014.02.04.
+ *      Author: kisstom
+ */
+
+#include "logger_factory.h"
+
+LoggerFactory::LoggerFactory(string debugLevel, string appenderType,
+		string logfile_name = "") {
+	log4cpp::Category& root = log4cpp::Category::getRoot();
+
+	if (debugLevel.compare("INFO") == 0) {
+		root.setPriority(log4cpp::Priority::INFO);
+	} else if (debugLevel.compare("DEBUG") == 0) {
+		root.setPriority(log4cpp::Priority::DEBUG);
+	} else if (debugLevel.compare("ERROR") == 0) {
+		root.setPriority(log4cpp::Priority::ERROR);
+	} else {
+		root.setPriority(log4cpp::Priority::INFO);
+	}
+	log4cpp::Appender *appender;
+
+	if (appenderType.compare("CONSOLE") == 0) {
+		appender = new log4cpp::OstreamAppender("default", &std::cout);
+	} else if (appenderType.compare("FILE") == 0) {
+		appender = new log4cpp::FileAppender("default", logfile_name);
+	} else {
+		// TODO
+	}
+
+	log4cpp::PatternLayout * layout = new log4cpp::PatternLayout();
+	layout->setConversionPattern("%d{%H:%M:%S,%l} %c %x: %m\n");
+	appender->setLayout(layout);
+
+	root.addAppender(appender);
+}
---
+++ src/cpp/main/common/util/logger_factory.h
@@ -0,0 +1,30 @@
+/*
+ * logger_factory.h
+ *
+ *  Created on: 2014.02.04.
+ *      Author: kisstom
+ */
+
+#ifndef LOGGER_FACTORY_H_
+#define LOGGER_FACTORY_H_
+
+#include "log4cpp/Category.hh"
+#include "log4cpp/Appender.hh"
+#include "log4cpp/FileAppender.hh"
+#include "log4cpp/OstreamAppender.hh"
+#include "log4cpp/Layout.hh"
+#include "log4cpp/PatternLayout.hh"
+#include "log4cpp/BasicLayout.hh"
+#include "log4cpp/Priority.hh"
+
+#include <string>
+
+using std::string;
+
+class LoggerFactory {
+public:
+	LoggerFactory(string level, string appender, string logfilename);
+};
+
+
+#endif /* LOGGER_FACTORY_H_ */
