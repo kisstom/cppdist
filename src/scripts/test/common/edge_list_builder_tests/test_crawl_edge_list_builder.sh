f9d30097466b9a3c1c5961354a910c6e7945d6fd
kisstom# <kisstom@info.ilab.sztaki.hu>
2014-05-07 15:27:27 +0200

    edge list builder test shs ready

--- src/scripts/test/common/edge_list_builder_tests/test_crawl_edge_list_builder.sh
+++ src/scripts/test/common/edge_list_builder_tests/test_crawl_edge_list_builder.sh
@@ -1,25 +1,26 @@
 #!/bin/bash -eu
 
-exit 0
 trap 'echo "E: Crawl edge list builder test failed with exitcode $?"' ERR
 
 this_dir="$(dirname "$0")"
 input=$this_dir/../../../../../resources/edge_list_builder_test/graph
+expectedCrawl=$this_dir/../../../../../resources/edge_list_builder_test/expectedCrawl
+maxNodeToKeep=2
 
 work_dir=`mktemp -d`
 output=$work_dir/outputGraph
 
-$this_dir/../../../../bin/test/useFileSystem/common/edge_list_builder_tests/edge_list_builder_main $input $output 
-output_diff=`diff $input $output`
+$this_dir/../../../../bin/test/useFileSystem/common/edge_list_builder_tests/crawl_edge_list_builder_main $maxNodeToKeep  $input $output 
+output_diff=`diff $expectedCrawl $output`
 
 if [ "$output_diff" != "" ]; then
-  echo Filter test failed. Diff:
+  echo Crawl edge list builder test failed. Diff:
   echo $output_diff
   exit 1
 fi
 
 rm -rf "$work_dir"
 
-echo "Edge list builder test accepted."
+echo "Crawl edge list builder test accepted."
 
 exit 0
--- src/scripts/test/common/edge_list_builder_tests/test_filter_edge_list_builder.sh
+++ src/scripts/test/common/edge_list_builder_tests/test_filter_edge_list_builder.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eu
 
-exit 0
 trap 'echo "E: Filter edge list builder test failed with exitcode $?"' ERR
 
 this_dir="$(dirname "$0")"
 input=$this_dir/../../../../../resources/edge_list_builder_test/graph
 filterNodeFile=$this_dir/../../../../../resources/edge_list_builder_test/filterNode
+expectedFiltered=$this_dir/../../../../../resources/edge_list_builder_test/expectedFiltered
 
 work_dir=`mktemp -d`
 output=$work_dir/outputGraph
 
 $this_dir/../../../../bin/test/useFileSystem/common/edge_list_builder_tests/filter_edge_list_builder_main $filterNodeFile  $input $output 
-output_diff=`diff $input $output`
+output_diff=`diff $expectedFiltered $output`
 
 if [ "$output_diff" != "" ]; then
   echo Filter edge list build test failed. Diff:
@@ -21,5 +21,5 @@ fi
 
 rm -rf "$work_dir"
 
-echo "Edge list builder test accepted."
+echo "Filter edge list builder test accepted."
 exit 0
