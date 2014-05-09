8f56f0f7c8231897032e2790e1c3e922baef2a89
kisstom# <kisstom@info.ilab.sztaki.hu>
2014-05-09 16:37:20 +0200

    adding deb msg to prnode

--- src/cpp/main/algos/pagerank/pagerank_node.cpp
+++ src/cpp/main/algos/pagerank/pagerank_node.cpp
@@ -19,6 +19,7 @@ PagerankNode::PagerankNode(int maxIter, long allNode, double dump) {
 }
 
 void PagerankNode::beforeIteration() {
+  logger_->info("Starting iteration %d", actIter);
   for (long node = 0; node < matrix_->getNumberOfNodes(); ++node) {
     (*tmpScore_)[node] = 0.0;
   }
@@ -26,7 +27,6 @@ void PagerankNode::beforeIteration() {
 
 bool PagerankNode::afterIteration() {
   for (long node = 0; node < (long) tmpScore_->size(); ++node) {
-    logger_->info("tmp score %ld %lf", node, (*tmpScore_)[node]);
     (*pagerankScore_)[node] = (*tmpScore_)[node] * (1.0 - dump_) + dump_ / allNode_;
     (*tmpScore_)[node] = 0.0;
   }
@@ -34,7 +34,7 @@ bool PagerankNode::afterIteration() {
 }
 
 void PagerankNode::sender() {
-  long outEdge, numNeighbors, partitionNode;
+  long outEdge, numNeighbors;
   int partIndex;
 
   for (long partitionNode = 0; partitionNode < matrix_->getNumberOfNodes(); ++partitionNode) {
