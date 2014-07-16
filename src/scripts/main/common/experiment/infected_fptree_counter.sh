694bcec0738a250d588f68ac2bee682aa161465f
kisstom <tamkis1@gmail.com>
2014-07-16 15:10:30 +0200

    commit in hulk2

--- src/scripts/main/common/experiment/infected_fptree_counter.sh
+++ src/scripts/main/common/experiment/infected_fptree_counter.sh
@@ -5,4 +5,6 @@ outdir=$1
 
 . $thisDir/../tools/create_experiment_info.sh
 
+echo $LD_LIBRARY_PATH
+
 "$thisDir"/../../../../bin/main/dmoz/infectedTreeComputerJob /mnt/idms/_bck/store/kisstom/data/portugal/reindexed.nopara.inv /mnt/idms/kisstom/run/distributed/portugal/simrank_odd_even/fptree_infection/simrank_on_top200M/concat.txt "$outdir"/output.txt 10 200000000 205000000 210000000 215000000 220000000 225000000 230000000 235000000 240000000 245000000
