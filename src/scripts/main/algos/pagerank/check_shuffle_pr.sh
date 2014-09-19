7112e288ec606ca1d374b77351ec7bb5706ef344
kisstom# <kisstom@info.ilab.sztaki.hu>
2014-09-19 16:35:07 +0200

    shuffle test still in late

--- src/scripts/main/algos/pagerank/check_shuffle_pr.sh
+++ src/scripts/main/algos/pagerank/check_shuffle_pr.sh
@@ -1,14 +1,55 @@
 #!/bin/bash -eu
 
-workdir=`mktemp`
+#trap 'ls $workdir' EXIT
+workdir=`mktemp -d`
 
-thisDir=$(dirnam $0)
+thisDir=$(readlink -f $(dirname $0))
 
-origPr=$1
-newPr=$2
-shuffleIndices=$3
+echo $thisDir
 
-awk -v shuffleIndexFile=$shuffleIndices -f $thisDir/get_orig_pr.awk $newPr > $workdir/testOrigPr
-head -1000 $workdir/testOrigPr > $workdir/testOrigPr.h1000
-head -1000 $origPr > $workdir/origPr.h1000
-awk -v egyik=$workdir/origPr.h1000 -f $thisDir/../common/measures/rmse.awk $workdir/testOrigPr.h1000
+resourceDir=$thisDir/../../../../../resources
+defaultPagerankIni=$resourceDir/integration/customPagerank.ini
+customPagerankIni=$workdir/customPagerank.ini
+shuffleCustomPagerankIni=$workdir/shuffleCustomPagerank.ini
+inputData=$resourceDir/filter_test/small.txt
+binDir=$thisDir/../../../../../bin/
+scriptDir=$thisDir/../../../../../scripts/
+
+
+localDir=$workdir/"outdir"
+remoteDir=$workdir/"partitions"
+inversePartDir=$remoteDir/"inversePart"
+slaveryCfg=$remoteDir/"slavery.cfg"
+
+$thisDir/../../common/tools/converters/shuffle_reindexer.sh $inputData 2 $workdir/shuffleDir
+shuffleData=$workdir/shuffleDir/shuffle_reindex.clue
+
+pushd $thisDir/../
+
+	echo cat $defaultPagerankIni|\
+	sed 's/\(INPUT_DATA=\).*/\1'$inputData'/'|\
+	sed 's/\(BASE_DIR=\).*/\1'$workdir'/'|\
+	sed 's/\(REMOTE_DIR=\).*/\1'$remoteDir'/'|\
+	sed 's/\(INVERSE_PARTITION_DIR=\).*/\1'$inversePartDir'/'|\
+	sed 's/\(SLAVE_CONFIG=\).*/\1'$slaveryCfg'/'|\
+	sed 's/\(BIN=\).*/\1'$binDir'/'|\
+	sed 's/\(SCRIPTDIR=\).*/\1'$scriptDir'/' > $customPagerankIni
+
+	exit
+	fab cfg:$customPagerankIni computeAll
+	cat $outputDir/out_* > $workdir/custom_concat
+
+	cat $customPagerankIni|\
+	sed 's/\(INPUT_DATA=\).*/\1'$shuffleData'/' > $shuffleCustomPagerankIni
+
+	fab cfg:$shuffleCustomPagerankIni computeAll
+
+	cat $outputDir/out_* > $workdir/custom_shuffle_concat
+popd
+
+shuffleIndices=$workdir/shuffleDir/shuffle_reindex.sort
+
+awk -v shuffleIndexFile=$shuffleIndices -f $thisDir/get_orig_pr.awk $workdir/custom_shuffle_concat \
+> $workdir/testOrigPr
+
+awk -v egyik=$workdir/testOrigPr -f $thisDir/../common/measures/rmse.awk $workdir/custom_concat
