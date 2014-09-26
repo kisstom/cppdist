f7eb69999794eea5b74ff363fa3e868910c1c3c8
kisstom# <kisstom@info.ilab.sztaki.hu>
2014-09-26 13:30:49 +0200

    to test

--- src/cpp/main/algos/counter_inverse/counter_inverse_node.h
+++ src/cpp/main/algos/counter_inverse/counter_inverse_node.h
@@ -20,8 +20,8 @@ struct InverseTriple {
     count(c), to(t), fromPartition(_fromPartition) {}
 
   static bool compare(const InverseTriple& lh, const InverseTriple& rh) {
-    if (lh.fromPartition == rh.fromPartition) return lh.to < rh.to;
-    return lh.fromPartition < rh.fromPartition;
+    if (lh.to == rh.to) return lh.fromPartition == rh.fromPartition;
+    return lh.to < rh.to;
   }
 
   long count;
