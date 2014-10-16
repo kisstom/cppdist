007c38a312f5a6bf4876e37208d4a47425b5e4c9
kisstom <tamkis1@gmail.com>
2014-10-16 09:41:57 +0200

    fixing time conversion in sender time print

--- src/scripts/main/common/tools/get_runtime.sh
+++ src/scripts/main/common/tools/get_runtime.sh
@@ -2,14 +2,14 @@
 
 logDir=$1
 findIterAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/find_after_iter.awk"
-printSenderAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/print_sender_time.awk"
+printSenderAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/print_sender_time_milli.awk"
 
 for ind in `seq 0 9`
 do 
   for x in `seq 0 19`
   do
     awk -v ind=$ind -f "$findIterAwk" "$logDir"/log_$x |\
-               egrep "(R|r)eceiver" #| awk -f "$printSenderAwk"
+               egrep "(R|r)eceiver" | awk -f "$printSenderAwk"
   done
   echo "----------------------------------"
 done
