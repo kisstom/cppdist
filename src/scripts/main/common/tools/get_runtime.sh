b9cab49a13840d1a39d5638eda1f6bc9954d08ee
kisstom <tamkis1@gmail.com>
2014-10-27 15:19:23 +0100

    refactoring iter time counter

--- src/scripts/main/common/tools/get_runtime.sh
+++ src/scripts/main/common/tools/get_runtime.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -eu
 
 logDir=$1
-findIterAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/find_after_iter.awk"
+findIterAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/find_iter_gap.awk"
 printSenderAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/print_sender_time_milli.awk"
 
 for ind in `seq 0 9`
 do 
   for x in `seq 0 19`
   do
-    awk -v ind=$ind -f "$findIterAwk" "$logDir"/log_$x |\
-               egrep "(R|r)eceiver" | awk -f "$printSenderAwk"
+    awk -v ind=$ind -f "$findIterAwk" "$logDir"/log_$x | awk -f "$printSenderAwk"
   done
   echo "----------------------------------"
 done
