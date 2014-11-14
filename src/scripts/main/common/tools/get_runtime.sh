af3b58d958eadebef72164145179c154bdde596f
kisstom# <kisstom@info.ilab.sztaki.hu>
2014-11-14 11:35:34 +0100

    comment on get runtime

--- src/scripts/main/common/tools/get_runtime.sh
+++ src/scripts/main/common/tools/get_runtime.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -eu
 
 logDir=$1
-findIterAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/find_iter_gap.awk"
-printSenderAwk="/home/kisstom/git/Cppdist/src/scripts/main/common/tools/print_sender_time_milli.awk"
 numPart=$2
+
+baseDir=$(readlink -f `dirname install_gtest.sh`)
+findIterAwk="$baseDir/find_iter_gap.awk"
+printSenderAwk="$baseDir/print_sender_time_milli.awk"
 
 cumulate=0.0
 for ind in `seq 0 9`
@@ -11,14 +13,16 @@ do
   max=0 
   for x in `seq 0 $numPart`
   do
+    # Finds the starting and ending time of the ind. iteration for the 
+    # x. node, and counts the difference.
     time=`awk -v ind=$ind -f "$findIterAwk" "$logDir"/log_$x | awk -f "$printSenderAwk"`
     compare=`echo "$time>$max"|bc -l`
     if [ "$compare" == "1" ]; then
       max=$time
     fi
   done
+  # Sums the maximum of each iteration.
   cumulate=`echo "$cumulate+$max"|bc -l`
-#  echo $cumulate
 done
 
 echo "$cumulate/10"|bc -l
