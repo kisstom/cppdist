b5d3beff8b305518e4528f0f21ad539446daea4c
kisstom <tamkis1@gmail.com>
2015-02-05 10:39:32 +0100

    fixing bug in install dep

--- src/scripts/main/common/tools/install_dependencies.sh
+++ src/scripts/main/common/tools/install_dependencies.sh
@@ -4,9 +4,9 @@ trap "echo Some errors occurred while installing dependencies. Exiting." ERR
 
 SCRIPT_DIR=$(readlink -f $(dirname "$0"))
 
-SCRIPT_DIR/install_gmock.sh
-SCRIPT_DIR/install_gmp.sh
-SCRIPT_DIR/install_gsl.sh
-SCRIPT_DIR/install_gtest.sh
-SCRIPT_DIR/install_logcpp.sh 
-SCRIPT_DIR/install_zmq.sh
+$SCRIPT_DIR/install_gmock.sh
+$SCRIPT_DIR/install_gmp.sh
+$SCRIPT_DIR/install_gsl.sh
+$SCRIPT_DIR/install_gtest.sh
+$SCRIPT_DIR/install_logcpp.sh 
+$SCRIPT_DIR/install_zmq.sh
